e7dd7e70f57d433b89591638c1f89447f90e0f89
elias <14819853+eliasthecactus@users.noreply.github.com>
2024-01-25 09:44:14 -0800

    login/signup changes

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,53 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import { AlertService } from '../../services/alert.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterOutlet, RouterLinkActive, HttpClientModule, FormsModule],
   templateUrl: './login.component.html',
+  providers: [ApiService],
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loadingSpinner: boolean = false;
+  emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  userData: any = {
+    email: '',
+    password: ''
+  };
+  
+
+  constructor(private apiService: ApiService, public alertService: AlertService) {
+
+  }
 
   login() {
-    console.log("check")
+
+
+    this.loadingSpinner = true;
+    document.getElementById('loginButton')?.classList.add("btn-disabled")
+
+    this.apiService.signup(this.userData).subscribe(
+      (response) => {
+        console.log(response);
+        this.loadingSpinner = false;
+        document.getElementById('loginButton')?.classList.remove("btn-disabled")
+        this.alertService.show("success", "Success")
+      },
+      (error) => {
+        console.log(error);
+        this.loadingSpinner = false;
+        document.getElementById('loginButton')?.classList.remove("btn-disabled")
+        this.alertService.show("error", "There was an error while logging in")
+      }
+    );
   }
 
 }
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -3,39 +3,96 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from '../../services/api.service';
-import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { AlertService } from '../../services/alert.service';
 
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, RouterLinkActive, HttpClientModule],
+  imports: [CommonModule, RouterLink, RouterOutlet, RouterLinkActive, HttpClientModule, FormsModule],
   templateUrl: './signup.component.html',
   providers: [ApiService],
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
-userData: any;
+  loadingSpinner: boolean = false;
+  emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-  constructor(private apiService: ApiService) {}
+  userData: any = {
+    first_name: '',
+    last_name: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  };
+
+  constructor(private apiService: ApiService, public alertService: AlertService) {}
 
   signup() {
-    const userData = {
-      first_name: '',
-      last_name: '',
-      username: '',
-      email: '',
-      password: ''
-    };
+    var accept_tou = (document.getElementById('registerAcceptToU') as HTMLInputElement).checked;
 
+    if (!accept_tou) {
+      this.alertService.show("error", "Please accept the Terms of Use")
+      return
+    }
 
-    this.apiService.signup(userData).subscribe(
+    if (this.userData.first_name.length < 1) {
+      this.alertService.show("error", "Please fill in the first name")
+      return
+    }
+
+    if (this.userData.last_name.length < 1) {
+      this.alertService.show("error", "Please fill in the last name")
+      return
+    }
+
+    if (this.userData.username.length < 5) {
+      this.alertService.show("error", "Please use at least 5 characters for the username")
+      return
+    }
+
+    if (this.userData.email.length < 1) {
+      this.alertService.show("error", "Please fill in a real email")
+      return
+    }
+
+    if (this.userData.password.length < 8) {
+      this.alertService.show("error", "Please use at least 8 characters for the password")
+      return
+    }
+
+    if (this.userData.password != this.userData.confirmPassword) {
+      this.alertService.show("error", "Password do not match")
+      return
+    }
+
+    if (!this.emailPattern.test(this.userData.email)) {
+      this.alertService.show("error", "Invalid email address")
+      return
+    }
+
+    
+    
+    
+    this.loadingSpinner = true;
+    document.getElementById('signupButton')?.classList.add("btn-disabled")
+
+    this.apiService.signup(this.userData).subscribe(
       (response) => {
-        console.log('Signup successful', response);
+        console.log(response);
+        this.loadingSpinner = false;
+        document.getElementById('signupButton')?.classList.remove("btn-disabled")
+        this.alertService.show("success", "Sign up was successfull")
       },
       (error) => {
-        console.error('Signup failed', error);
+        console.log(error);
+        this.loadingSpinner = false;
+        document.getElementById('signupButton')?.classList.remove("btn-disabled")
+        this.alertService.show("error", "There was an error")
       }
     );
   }
+
 }
