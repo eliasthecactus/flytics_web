dc1f5fde51d0d3ce36f1f25175eabe36fb39d231
elias <14819853+eliasthecactus@users.noreply.github.com>
2024-01-30 00:16:52 +0100

    upload and password reset

--- src/app/pages/add-flight/add-flight.component.ts
+++ src/app/pages/add-flight/add-flight.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TokenCheckerService } from '../../services/token-checker.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-add-flight',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './add-flight.component.html',
+  providers: [TokenCheckerService, ApiService],
   styleUrl: './add-flight.component.css'
 })
 export class AddFlightComponent {
+
+  constructor(private tokenChecker: TokenCheckerService) {}
+
+  folderUpload: boolean = false;
+  kmlFiles: File[] = [];
+  privateFlight: boolean = false;
+  flightInformation: String = '';
+
+  onFileSelected(event: any) {
+    const selectedFiles: FileList = event.target.files;
+    this.kmlFiles = Array.from(selectedFiles).filter(file => {
+      return (
+        file.name.toLowerCase().endsWith('.kml') &&
+        file.size <= 1024 * 1024 // 1MB in bytes
+      );
+    });
+    // console.log('Selected Files:', this.kmlFiles);
+  }
+
+  toggleUploadType() {
+    this.folderUpload = !this.folderUpload;
+  }
+
+  ngOnInit() {
+    this.tokenChecker.redirectToLoginIfExpired();
+  }
 
 
 
--- src/app/pages/password-reset/password-reset.component.ts
+++ src/app/pages/password-reset/password-reset.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { AlertService } from '../../services/alert.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-password-reset',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './password-reset.component.html',
   styleUrl: './password-reset.component.css'
 })
 export class PasswordResetComponent {
+
+  loadingSpinner: boolean = false;
+  email: String = "";
+  password: String = "";
+
+  constructor(public alertService: AlertService) {}
 
 }
